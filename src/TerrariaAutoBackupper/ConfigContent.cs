887ebe7305969ca399c40840dc55132f4fceda46
Eixini <eixi16@gmail.com>
2023-12-08 19:45:59 +0300

    Add project files

---
+++ src/TerrariaAutoBackupper/ConfigContent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerrariaAutoBackupper;
+
+internal class ConfigContent
+{
+    // Директории, где находятся данные игры - персонажи, миры и т.д
+    public string? GameDataDirectory { get; set; }
+
+    // Целевые директории, куда нужно бекапить данные
+    public string? BackupTargetDirectory { get; set; }
+
+    // Список персонажей, файлы которых нужно бекапить
+    public List<string>? BackupPlayersTargets { get; set; }
+    public List<string>? BackupWorldsTargets { get; set; }
+
+    // Конфигурация самого приложения
+    public bool LaunchSystemStartup { get; set; }
+}
---
+++ src/TerrariaAutoBackupper/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace TerrariaAutoBackupper;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Backupper backupper = new Backupper();
+    }
+}
