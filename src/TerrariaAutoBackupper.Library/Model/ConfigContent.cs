95172d7cba2bcb912fddf969589ff5ec4bf56648
Eixini <eixi16@gmail.com>
2023-12-17 20:41:39 +0300

    Change options

---
+++ src/TerrariaAutoBackupper.Library/Model/ConfigContent.cs
@@ -0,0 +1,21 @@
+﻿namespace TerrariaAutoBackupper.Library.Model;
+
+public class ConfigContent
+{
+    // Директории, где находятся данные игры - персонажи, миры и т.д
+    public string? GameDataDirectory { get; set; }
+
+    // Целевые директории, куда нужно бекапить данные
+    public string? BackupTargetDirectory { get; set; }
+
+    // Список персонажей, файлы которых нужно бекапить
+    public List<string>? BackupPlayersTargets { get; set; }
+    public List<string>? BackupWorldsTargets { get; set; }
+
+    // Конфигурация самого приложения
+    public bool LaunchSystemStartup { get; set; }
+
+    // Список доступных языков
+    public List<string>? AvailableLanguages { get; set; }
+    public string? SelectedLanguage { get; set; }
+}
